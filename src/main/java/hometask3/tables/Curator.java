a1be6254c8f73312e557d77f8cee6f581fd5594d
Evgeniy Alferyev <evgeniy.alferev@ligastavok.ru>
2022-10-08 11:57:09 +0500

    add new classes for tables

---
+++ src/main/java/hometask3/tables/Curator.java
@@ -0,0 +1,9 @@
+package hometask3.tables;
+
+public class Curator extends AbsTable{
+
+    public Curator(String tableName) {
+        super("curators");
+    }
+
+}
---
+++ src/main/java/hometask3/tables/Group.java
@@ -0,0 +1,10 @@
+package hometask3.tables;
+
+public class Group extends AbsTable{
+
+    public Group(String tableName) {
+        super("groups");
+    }
+
+
+}
